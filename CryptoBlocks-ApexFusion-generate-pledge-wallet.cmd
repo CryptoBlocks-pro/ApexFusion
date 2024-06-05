4aedf7447e9b2387ba9584d96a0f84da14f7b503
Eric Long <eric@cryptoblocks.pro>
2024-06-05 16:53:14 -0700

    Initial script

--- CryptoBlocks-ApexFusion-generate-pledge-wallet.cmd
+++ CryptoBlocks-ApexFusion-generate-pledge-wallet.cmd
@@ -18,15 +18,26 @@ dir /b | findstr /b "%walletName%" >nul 2>&1
 IF %ERRORLEVEL% EQU 1 (
     echo No files starting with %walletName% found.
 ) ELSE (
-    echo Files starting with %walletName% found.
-    echo WARNING: If you continue, the mnemonic key word phrase will be permanently overwritten and wallet recovery will be impossible.
-    echo Are you sure you want to overwrite these files? [Y/N]
+    echo +***********************************************************************************************************+
+    echo Existing files starting with %walletName% found.
+    echo WARNING: If you continue, the mnemonic key word phrase and all generated files will be permanently overwritten.
+    echo Is this what you intended?
+    echo. 
+    echo If you are creating a multi-owner pool and/or have a separate reward wallet from the owner wallet, cancel
+    echo this script and use a different wallet name for the new wallet. Each wallet must have a unique name if you need
+    echo more than one wallet.
+    echo.
+    echo Are you sure you want to overwrite these files?
+    echo +***********************************************************************************************************+
     choice /C YN /M "Press Y to overwrite, N to cancel"
     IF ERRORLEVEL 2 (
         echo Please run again with a different wallet name or delete all of the files with the current wallet name if you want to try again.
         exit /b
     )
 )
+
+rem clear the screen for less clutter
+cls
 
 rem Define the source URLs
 SET cardanoNodeSource=https://github.com/IntersectMBO/cardano-node/releases/download/8.9.3/cardano-node-8.9.3-win64.zip
@@ -90,10 +101,10 @@ rem Get root private key from mnemonic phrase
 type %walletName%.mnemonic.txt | %CARDANO_ADDRESS% key from-recovery-phrase Shelley > %walletName%.root.prv
 
 rem Derive stake key from root private key
-type %walletName%.root.prv | %CARDANO_ADDRESS% key child 1852H/1815H/0H/2/0 > %walletName%.staking.xprv
+type %walletName%.root.prv | %CARDANO_ADDRESS% key child 1852H/1815H/0H/2/0 > %walletName%.stake.xprv
 
 rem Retrieve stake public key from stake private key
-type %walletName%.staking.xprv | %CARDANO_ADDRESS% key public --with-chain-code > %walletName%.staking.xpub
+type %walletName%.stake.xprv | %CARDANO_ADDRESS% key public --with-chain-code > %walletName%.stake.xpub
 
 rem Derive payment key from root private key
 type %walletName%.root.prv | %CARDANO_ADDRESS% key child 1852H/1815H/0H/0/0 > %walletName%.payment.xprv
@@ -104,17 +115,17 @@ type %walletName%.payment.xprv | %CARDANO_ADDRESS% key public --with-chain-code 
 rem Get payment address
 type %walletName%.payment.xpub | %CARDANO_ADDRESS% address payment --network-tag 0 > %walletName%.payment.addr_candidate
 
-set /p staking.xpub= < %walletName%.staking.xpub
+set /p stake.xpub= < %walletName%.stake.xpub
 
 rem Calculate a base.addr candidate to match at the end
-type %walletName%.payment.addr_candidate | %CARDANO_ADDRESS% address delegation %staking.xpub% > %walletName%.base.addr_candidate
+type %walletName%.payment.addr_candidate | %CARDANO_ADDRESS% address delegation %stake.xpub% > %walletName%.base.addr_candidate
 
 rem Create SESKEY
-type %walletName%.staking.xprv | %BECH32% > %walletName%.staking.xprvb32
-set /p staking.xprvb32= < %walletName%.staking.xprvb32
-type %walletName%.staking.xpub | %BECH32% > %walletName%.staking.xpubb32
-set /p staking.xpubb32= < %walletName%.staking.xpubb32
-set SESKEY=%staking.xprvb32:~0,128%%staking.xpubb32%
+type %walletName%.stake.xprv | %BECH32% > %walletName%.stake.xprvb32
+set /p stake.xprvb32= < %walletName%.stake.xprvb32
+type %walletName%.stake.xpub | %BECH32% > %walletName%.stake.xpubb32
+set /p stake.xpubb32= < %walletName%.stake.xpubb32
+set SESKEY=%stake.xprvb32:~0,128%%stake.xpubb32%
 
 rem Create PESKEY
 type %walletName%.payment.xprv | %BECH32% > %walletName%.payment.xprvb32
@@ -123,20 +134,20 @@ type %walletName%.payment.xpub | %BECH32% > %walletName%.payment.xpubb32
 set /p payment.xpubb32= < %walletName%.payment.xpubb32
 set PESKEY=%payment.xprvb32:~0,128%%payment.xpubb32%
 
-rem Generate staking skey
-echo {^"type^": ^"StakeExtendedSigningKeyShelley_ed25519_bip32^", ^"description^": ^"^", ^"cborHex^": ^"5880%SESKEY%^"} > %walletName%.staking.skey
+rem Generate stake skey
+echo {^"type^": ^"StakeExtendedSigningKeyShelley_ed25519_bip32^", ^"description^": ^"^", ^"cborHex^": ^"5880%SESKEY%^"} > %walletName%.stake.skey
 
 rem Generate payment skey
 echo {^"type^": ^"PaymentExtendedSigningKeyShelley_ed25519_bip32^", ^"description^": ^"Payment Signing Key^", ^"cborHex^": ^"5880%PESKEY%^"} > %walletName%.payment.skey
 
-%CARDANO_CLI% shelley key verification-key --signing-key-file %walletName%.staking.skey --verification-key-file %walletName%.staking.evkey
-%CARDANO_CLI% shelley key non-extended-key --extended-verification-key-file %walletName%.staking.evkey --verification-key-file %walletName%.staking.vkey
+%CARDANO_CLI% shelley key verification-key --signing-key-file %walletName%.stake.skey --verification-key-file %walletName%.stake.evkey
+%CARDANO_CLI% shelley key non-extended-key --extended-verification-key-file %walletName%.stake.evkey --verification-key-file %walletName%.stake.vkey
 %CARDANO_CLI% shelley key verification-key --signing-key-file %walletName%.payment.skey --verification-key-file %walletName%.payment.evkey
 %CARDANO_CLI% shelley key non-extended-key --extended-verification-key-file %walletName%.payment.evkey --verification-key-file %walletName%.payment.vkey
 
-%CARDANO_CLI% shelley stake-address build --stake-verification-key-file %walletName%.staking.vkey --testnet-magic 3311 > %walletName%.staking.addr
+%CARDANO_CLI% shelley stake-address build --stake-verification-key-file %walletName%.stake.vkey --testnet-magic 3311 > %walletName%.reward.addr
 %CARDANO_CLI% shelley address build --payment-verification-key-file %walletName%.payment.vkey --testnet-magic 3311 > %walletName%.payment.addr
-%CARDANO_CLI% shelley address build --payment-verification-key-file %walletName%.payment.vkey --stake-verification-key-file %walletName%.staking.vkey --testnet-magic 3311 > %walletName%.base.addr
+%CARDANO_CLI% shelley address build --payment-verification-key-file %walletName%.payment.vkey --stake-verification-key-file %walletName%.stake.vkey --testnet-magic 3311 > %walletName%.base.addr
 
 echo.
 echo Downloads are complete. Creating wallet...
@@ -151,12 +162,15 @@ type %walletName%.payment.addr
 echo.
 echo +-----------------------------------------------------------------------------------------------------------+
 echo.
-echo Do the addresses match? [Y/N]
+echo Do the addresses match?
 choice /C YN /M "Press Y if they match, N if they don't"
 IF ERRORLEVEL 2 (
     echo Please take a screenshot and send to admin@cryptoblock.pro for help troubleshooting.
     exit /b
 )
+rem clear the screen for less clutter
+cls
+
 
 echo The following two addresses must match otherwise things went wrong.
 echo +-----------------------------------------------------------------------------------------------------------+
@@ -168,12 +182,14 @@ type %walletName%.base.addr
 echo.
 echo +-----------------------------------------------------------------------------------------------------------+
 echo.
-echo Do the addresses match? [Y/N]
+echo Do the addresses match?
 choice /C YN /M "Press Y if they match, N if they don't"
 IF ERRORLEVEL 2 (
     echo Please take a screenshot and send to admin@cryptoblock.pro for help troubleshooting.
     exit /b
 )
+rem clear the screen for less clutter
+cls
 
 echo:
 echo Congratulations. Wallet is created.
@@ -184,14 +200,15 @@ echo:
 echo WARNING: Please safely record this phrase and DO NOT reveal it to anyone.
 echo This phrase is the only way to recover your wallet if you lose access to it.
 echo Losing this phrase is like losing your wallet keys - you will not be able to recover your wallet.
+echo And, it goes without saying, DO NOT share this phrase with anyone.
 echo +-----------------------------------------------------------------------------------------------------------+
 echo:
-echo Email these 4 specific files from your desktop folder to admin@cryptoblocks.pro or arrange to send via another means. This is the minum files required to register the wallet as a stake pool owner wallet, and it DOES NOT give access to spend or send funds.
+echo We will provide a link to a secure upload portal where you can upload the following files. If you have questions, you can email us at admin@cryptoblocks.pro or message us in our Discord server "apex-sp-hosting" channel at https://discord.com/channels/1247940142178762954/1247948945209032796. This is the minum files required to register the wallet as a stake pool owner wallet, and it DOES NOT give access to spend or send your funds.
 echo +-----------------------------------------------------------------------------------------------------------+
 echo - %walletName%.base.addr - Your wallet address. Allows Cryptoblocks to monitor the wallet to ensure it contains enough pledge. This is also the place where rewards can be paid out to.
-echo - %walletName%.staking.addr - Staking address that gets registered on the blockchain so it can be used as pledge.
-echo - %walletName%.staking.skey - The key used to sign the pool certificate and delegate to the pool.
-echo - %walletName%.staking.vkey - The public staking key
+echo - %walletName%.reward.addr - Staking address that gets registered on the blockchain so it can be used as pledge.
+echo - %walletName%.stake.skey - The key used to sign the pool certificate and delegate to the pool.
+echo - %walletName%.stake.vkey - The public staking key
 echo +-----------------------------------------------------------------------------------------------------------+
 echo:
 
